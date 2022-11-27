e137a0c52c75c6196c07636b4b29c1bf7dd7510a
Bishwajit <bishwa420@gmail.com>
2022-11-27 23:03:35 +0700

    - Animal detail view added (rest constituent items to be added later) - Navigation link added from animal list view to animal detail view

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -22,7 +22,12 @@ struct ContentView: View {
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
 
                 ForEach(animals) { animal in
-                    AnimalListItemView(animal: animal)
+                    NavigationLink(
+                        destination: AnimalDetailView(
+                            animal: animal)
+                    ) {
+                        AnimalListItemView(animal: animal)
+                    }
                 }
                 .listRowBackground(Color.black)
             } //: LIST
